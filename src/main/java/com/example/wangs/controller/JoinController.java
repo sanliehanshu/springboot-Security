8c888585ebb3e50f314d0ab58d6a4c792d5793d4
wangsheng <14213145@qq.com>
2020-05-13 18:45:35 +0800

    总结了resultType 用法，把查询结果封装到pojo或entiey类型, 实体对象的属性名和对应查询到的数据库的字段名要相同 @MapKey("id"),这样查询的时候用get id就可以了，特别方便

--- src/main/java/com/example/wangs/controller/JoinController.java
+++ src/main/java/com/example/wangs/controller/JoinController.java
@@ -1,17 +1,19 @@
 package com.example.wangs.controller;
 
 import com.example.wangs.entity.Product;
-import com.example.wangs.entity.Product_Category_vo;
 import com.example.wangs.entity.Role;
 import com.example.wangs.entity.User;
 import com.example.wangs.mapper.ProductMapper;
 import com.example.wangs.mapper.UserMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author wangsheng
@@ -22,6 +24,8 @@ public class JoinController {
     UserMapper usermapper;
     @Autowired
     ProductMapper productmapper;
+
+    Logger log = LoggerFactory.getLogger(JoinController.class);
 
 
     @RequestMapping("/test")
@@ -45,5 +49,40 @@ public class JoinController {
     public Product productListByName(@RequestParam String name){
         Product product = productmapper.selectByName(name);
         return product;
+    }
+    @RequestMapping("/productByName")
+    public String productByName(@RequestParam Integer id){
+        String product = productmapper.getProductById(id);
+        log.info("productByName:");
+        return product;
+    }
+
+    @RequestMapping("/getAllPro")
+    public List<Product>  getAllPro(){
+        List<Product> allProduct = productmapper.getAllProduct();
+        log.info("productByName:");
+        return allProduct;
+    }
+
+    @RequestMapping("/getAllProMap")
+    public Map<String, Object>  getAllProMap(Integer id){
+        Map<String, Object> proAsMapById = productmapper.getProAsMapById(id);
+        log.info("proAsMapById:");
+        return proAsMapById;
+    }
+
+    @RequestMapping("/getAllProsAsMap")
+    public Map<String, Product> getAllProsAsMap(){
+        Map<String, Product> proAsMapById = productmapper.getAllProsAsMap();
+        log.info("proAsMapById:");
+        return proAsMapById;
+    }
+
+
+    @RequestMapping("/getAllProsAsMapM")
+    Map<String, Map<String,Product>> getAllProsAsMapM(){
+        Map<String, Map<String, Product>> allProsAsMapM = productmapper.getAllProsAsMapM();
+        log.info("proAsMapById:");
+        return allProsAsMapM;
     }
 }
--- src/main/java/com/example/wangs/mapper/ProductMapper.java
+++ src/main/java/com/example/wangs/mapper/ProductMapper.java
@@ -1,9 +1,10 @@
 package com.example.wangs.mapper;
 
 import com.example.wangs.entity.Product;
+import org.apache.ibatis.annotations.MapKey;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -13,5 +14,17 @@ public interface ProductMapper {
 
     Product selectById(String id);
     Product selectByName(String name);
+    //测试resultType  返回String 测试Integer 自动转型String
+    String getProductById(Integer id);
+    List<Product> getAllProduct();
+    //  根据 id 查询信息，并把结果信息封装成 Map
+    Map<String, Object> getProAsMapById(Integer id);
+    // 查询所有员工的信息，把数据库中的 'id' 字段作为 key,对应的 value 封装成 Employee 对象
+    // @MapKey 中的值表示用数据库中的哪个字段名作 key
+    @MapKey("id")
+    Map<String, Product> getAllProsAsMap();
+
+    @MapKey("id")
+    Map<String, Map<String,Product>> getAllProsAsMapM();
 
 }
